129d78cc7812bbcb963d75c18c6513050cdea4dd
Лизуша <a123@Lizusha.local>
2020-04-08 22:10:37 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { ReactComponent as MyIcon} from './donut.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    count: 0
+  }
+
+  onDonutHandler = () =>{
+    this.setState({
+      count: this.state.count+1
+    })
+  }
+
+  onNotDonutHandler = () =>{
+    if(this.state.count > 0){
+      this.setState({
+        count: this.state.count-1
+      })
+    }
+    else{
+      this.setState({
+        count: 0
+      })
+    }
+  }
+
+
+  render(){
+    const imageClassName = this.state.count === 0 ? 'donut':'donutVisible'
+    const counterClassName = this.state.count === 0 ? 'counter':'counterVisible'
+    return(
+      <div>
+        <span className={counterClassName}>{this.state.count}</span>
+        <div>
+          <MyIcon className={imageClassName}/>
+          <div className='button'>
+            <button onClick={this.onDonutHandler}>DONUT</button>
+            <button onClick={this.onNotDonutHandler}>NOT DONUT</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
